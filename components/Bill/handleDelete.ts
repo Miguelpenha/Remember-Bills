e9273f84f7ffdc6f65fd61c1f39178f94ff7b81f
Miguel Penha <miguel.penha1@gmail.com>
2023-05-24 10:27:31 -0300

    Update handleDelete.ts

--- components/Bill/handleDelete.ts
+++ components/Bill/handleDelete.ts
@@ -1,7 +1,12 @@
 import api from '../../services/api/base'
+import { toast } from 'react-toastify'
 
 async function handleDelete(id: string) {
     await api.delete(`/bills/delete?bill=${id}`)
+
+    toast('Conta excluída', {
+        type: 'error'
+    })
 }
 
 export default handleDelete
