72bb9f7ca41d7a6a472f7534868cf7a26d2536a5
Miguel Penha <miguel.penha1@gmail.com>
2023-06-01 06:02:03 -0300

    Update [id].tsx

--- pages/edit/[id].tsx
+++ pages/edit/[id].tsx
@@ -95,7 +95,12 @@ function Edit() {
                             placeholder="Dono da conta..."
                         />
                     </Field>
-                    <ButtonSubmit type="submit" title="Editar"/>
+                    <ButtonSubmit type="submit" title="Editar">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <path d="M0 0h24v24H0z" fill="none"/>
+                            <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/>
+                        </svg>
+                    </ButtonSubmit>
                 </Form>
             </Container>
         </>
