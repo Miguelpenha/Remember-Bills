c5102562a0be0531aa6570ffd91363ebde5174d1
Miguel Penha <miguel.penha1@gmail.com>
2023-06-02 12:20:17 -0300

    Teste

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -34,8 +34,6 @@ export default class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900" rel="stylesheet"/>
                     <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon"/>
-                    <link rel="manifest" href="/manifest.json" crossOrigin="use-credentials"/>
-                    <meta name="theme-color" content="#0C1015"/>
                 </Head>
                 <body>
                     <Main/>
